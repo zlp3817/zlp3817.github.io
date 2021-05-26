c01aa06a72bd2e3db998ac2e4c508a55d34e84f1
Jerry <zlp3817@hotmail.com>
2021-05-26 22:11:14 +0800

    modify body and loose body

---
+++ common.js
@@ -0,0 +1,142 @@
+const hair_keyword = ['natural color', '613 color', 'shipping fee:', 'discount:', 'paypal fee:', 'making fee:'];
+const hair_type = ['bundle', 'closure', 'frontal', 'wig'];
+const hair_texture = ['straight', 'body wave', 'loose body wave', 'deep wave', 'water wave', 'curly', 'loose wave', 'kinky curly', 'kinky straight'];
+const hair_color = ['natural', '613', 'blond'];
+const closure_category = ['4x4', 'hd 5x5', '5x5 hd', '5x5', '6x6', '7x7'];
+const frontal_category = ['13x4', '13x6', '360'];
+const hair_length = ['10', '12', '14', '16', '18', '20', '22', '24', '26', '28', '30'];
+const hair_weight = { 'bundle': 110, 'closure': 45, 'frontal': 80, 'wig': 225 };
+
+const dic_hd_category = { '5x5 hd': 'hd 5x5' };
+
+const bundle_price = { '10': 18, '12': 22, '14': 26, '16': 30, '18': 34, '20': 38, '22': 44, '24': 49, '26': 57, '28': 62, '30': 66 };
+const bundle_price2 = { '10': 19, '12': 23, '14': 27, '16': 31, '18': 35, '20': 39, '22': 45, '24': 50, '26': 58, '28': 63, '30': 67 };
+const _4x4_price = { '10': 29, '12': 33, '14': 31, '16': 62, '18': 65, '20': 70 };
+const _5x5_price = { '10': 47, '12': 51, '14': 73, '16': 81, '18': 91, '20': 96 };
+const _13x4_price = { '10': 48, '12': 53, '14': 96, '16': 109, '18': 116, '20': 124 };
+const _13x6_price = { '10': 61, '12': 67, '14': 77, '16': 0, '18': 0, '20': 0 };
+const _360_price = { '10': 76, '12': 86, '14': 95, '16': 110, '18': 120, '20': 124 };
+const hd_5x5_price = { '10': 0, '12': 0, '14': 76, '16': 82, '18': 87, '20': 90 };
+
+const _613_bundle_price = { '10': 19, '12': 23, '14': 27, '16': 31, '18': 35, '20': 43, '22': 49, '24': 54, '26': 59, '28': 63, '30': 67 };
+const _613_4x4_price = { '10': 51, '12': 56, '14': 77, '16': 82, '18': 87, '20': 98 };
+const _613_5x5_price = { '10': 70, '12': 77, '14': 82, '16': 90, '18': 98, '20': 106 };
+const _613_13x4_price = { '10': 98, '12': 104, '14': 116, '16': 129, '18': 137, '20': 149 };
+const hair_price = {
+    'natural':
+    {
+        'bundle':
+        {
+            'straight': bundle_price,
+            'body wave': bundle_price,
+            'loose body wave': bundle_price2,
+            'deep wave': bundle_price2,
+            'water wave': bundle_price2,
+            'curly': bundle_price2,
+            'loose wave': bundle_price2,
+            'kinky curly': bundle_price2,
+            'kinky straight': bundle_price2,
+
+        },
+        'closure':
+        {
+            '4x4':
+            {
+                'straight': _4x4_price,
+                'body wave': _4x4_price,
+                'loose body wave': _4x4_price,
+                'deep wave': _4x4_price,
+                'water wave': _4x4_price,
+                'curly': _4x4_price,
+                'loose wave': _4x4_price,
+                'kinky curly': _4x4_price,
+                'kinky straight': _4x4_price,
+            },
+            '5x5':
+            {
+                'straight': _5x5_price,
+                'body wave': _5x5_price,
+                'loose body wave': _5x5_price,
+                'deep wave': _5x5_price,
+                'water wave': _5x5_price,
+                'curly': _5x5_price,
+                'loose wave': _5x5_price,
+                'kinky curly': _5x5_price,
+                'kinky straight': _5x5_price,
+            },
+            'hd 5x5':
+            {
+                'straight': hd_5x5_price,
+                'body wave': hd_5x5_price,
+                'loose body wave': hd_5x5_price,
+                'deep wave': hd_5x5_price,
+                'water wave': hd_5x5_price,
+                'curly': hd_5x5_price,
+                'loose wave': hd_5x5_price,
+                'kinky curly': hd_5x5_price,
+                'kinky straight': hd_5x5_price,
+            }
+        },
+        'frontal':
+        {
+            '13x4':
+            {
+                'straight': _13x4_price,
+                'body wave': _13x4_price,
+                'loose body wave': _13x4_price,
+                'deep wave': _13x4_price,
+                'water wave': _13x4_price,
+                'curly': _13x4_price,
+                'loose wave': _13x4_price,
+                'kinky curly': _13x4_price,
+                'kinky straight': _13x4_price,
+            },
+            '13x6':
+            {
+                'straight': _13x6_price,
+                'body bave': _13x6_price,
+                'loose body wave': _13x6_price,
+                'deep wave': _13x6_price,
+                'water wave': _13x6_price,
+                'curly': _13x6_price,
+                'loose wave': _13x6_price,
+                'kinky curly': _13x6_price,
+                'kinky straight': _13x6_price,
+            }
+        },
+        'wig':
+        {
+
+        },
+    },
+    '613':
+    {
+        'bundle':
+        {
+            'straight': _613_bundle_price,
+            'body wave': _613_bundle_price,
+
+        },
+        'closure':
+        {
+            '4x4':
+            {
+                'straight': _613_4x4_price,
+                'body wave': _613_4x4_price,
+            },
+            '5x5':
+            {
+                'straight': _613_5x5_price,
+                'body wave': _613_5x5_price,
+            }
+        },
+        'fontal':
+        {
+            '13x4':
+            {
+                'straight': _613_13x4_price,
+                'body wave': _613_13x4_price,
+            }
+        },
+    }
+};
--- main.js
+++ main.js
@@ -1,145 +1,3 @@
-var hair_keyword = ['natural color', '613 color', 'shipping fee:', 'discount:', 'paypal fee:', 'making fee:'];
-var hair_type = ['bundle', 'closure', 'frontal', 'wig'];
-var hair_texture = ['straight', 'body wave', 'loose body wave', 'deep wave', 'water wave', 'curly', 'loose wave', 'kinky curly', 'kinky straight'];
-var hair_color = ['natural', '613', 'blond'];
-var closure_category = ['4x4', 'hd 5x5', '5x5 hd', '5x5', '6x6', '7x7'];
-var frontal_category = ['13x4', '13x6', '360'];
-var hair_length = ['10', '12', '14', '16', '18', '20', '22', '24', '26', '28', '30'];
-var hair_weight = { 'bundle': 110, 'closure': 45, 'frontal': 80, 'wig': 225 };
-
-var dic_hd_category = { '5x5 hd': 'hd 5x5' };
-
-var bundle_price = { '10': 18, '12': 22, '14': 26, '16': 30, '18': 34, '20': 38, '22': 44, '24': 49, '26': 57, '28': 62, '30': 66 };
-var bundle_price2 = { '10': 19, '12': 23, '14': 27, '16': 31, '18': 35, '20': 43, '22': 49, '24': 54, '26': 59, '28': 63, '30': 67 };
-var _4x4_price = { '10': 29, '12': 33, '14': 31, '16': 62, '18': 65, '20': 70 };
-var _5x5_price = { '10': 47, '12': 51, '14': 73, '16': 81, '18': 91, '20': 96 };
-var _13x4_price = { '10': 48, '12': 53, '14': 96, '16': 109, '18': 116, '20': 124 };
-var _13x6_price = { '10': 61, '12': 67, '14': 77, '16': 0, '18': 0, '20': 0 };
-var _360_price = { '10': 76, '12': 86, '14': 95, '16': 110, '18': 120, '20': 124 };
-var hd_5x5_price = { '10': 0, '12': 0, '14': 76, '16': 82, '18': 87, '20': 90 };
-
-var _613_bundle_price = { '10': 19, '12': 23, '14': 27, '16': 31, '18': 35, '20': 43, '22': 49, '24': 54, '26': 59, '28': 63, '30': 67 };
-var _613_4x4_price = { '10': 51, '12': 56, '14': 77, '16': 82, '18': 87, '20': 98 };
-var _613_5x5_price = { '10': 70, '12': 77, '14': 82, '16': 90, '18': 98, '20': 106 };
-var _613_13x4_price = { '10': 98, '12': 104, '14': 116, '16': 129, '18': 137, '20': 149 };
-var hair_price = {
-    'natural':
-    {
-        'bundle':
-        {
-            'straight': bundle_price,
-            'body wave': bundle_price,
-            'loose body wave': bundle_price2,
-            'deep wave': bundle_price2,
-            'water wave': bundle_price2,
-            'curly': bundle_price2,
-            'loose wave': bundle_price2,
-            'kinky curly': bundle_price2,
-            'kinky straight': bundle_price2,
-
-        },
-        'closure':
-        {
-            '4x4':
-            {
-                'straight': _4x4_price,
-                'body wave': _4x4_price,
-                'loose body wave': _4x4_price,
-                'deep wave': _4x4_price,
-                'water wave': _4x4_price,
-                'curly': _4x4_price,
-                'loose wave': _4x4_price,
-                'kinky curly': _4x4_price,
-                'kinky straight': _4x4_price,
-            },
-            '5x5':
-            {
-                'straight': _5x5_price,
-                'body wave': _5x5_price,
-                'loose body wave': _5x5_price,
-                'deep wave': _5x5_price,
-                'water wave': _5x5_price,
-                'curly': _5x5_price,
-                'loose wave': _5x5_price,
-                'kinky curly': _5x5_price,
-                'kinky straight': _5x5_price,
-            },
-            'hd 5x5':
-            {
-                'straight': hd_5x5_price,
-                'body wave': hd_5x5_price,
-                'loose body wave': hd_5x5_price,
-                'deep wave': hd_5x5_price,
-                'water wave': hd_5x5_price,
-                'curly': hd_5x5_price,
-                'loose wave': hd_5x5_price,
-                'kinky curly': hd_5x5_price,
-                'kinky straight': hd_5x5_price,
-            }
-        },
-        'frontal':
-        {
-            '13x4':
-            {
-                'straight': _13x4_price,
-                'body wave': _13x4_price,
-                'loose body wave': _13x4_price,
-                'deep wave': _13x4_price,
-                'water wave': _13x4_price,
-                'curly': _13x4_price,
-                'loose wave': _13x4_price,
-                'kinky curly': _13x4_price,
-                'kinky straight': _13x4_price,
-            },
-            '13x6':
-            {
-                'straight': _13x6_price,
-                'body bave': _13x6_price,
-                'loose body wave': _13x6_price,
-                'deep wave': _13x6_price,
-                'water wave': _13x6_price,
-                'curly': _13x6_price,
-                'loose wave': _13x6_price,
-                'kinky curly': _13x6_price,
-                'kinky straight': _13x6_price,
-            }
-        },
-        'wig':
-        {
-
-        },
-    },
-    '613':
-    {
-        'bundle':
-        {
-            'straight': _613_bundle_price,
-            'body wave': _613_bundle_price,
-
-        },
-        'closure':
-        {
-            '4x4':
-            {
-                'straight': _613_4x4_price,
-                'body wave': _613_4x4_price,
-            },
-            '5x5':
-            {
-                'straight': _613_5x5_price,
-                'body wave': _613_5x5_price,
-            }
-        },
-        'fontal':
-        {
-            '13x4':
-            {
-                'straight': _613_13x4_price,
-                'body wave': _613_13x4_price,
-            }
-        },
-    }
-};
 function trim(str) {
     return str.replace(/\s+/g, '').toLocaleLowerCase();
 }
@@ -357,11 +215,21 @@ function unserialize(str) {
 //获取关键词
 function find_hair_keyword(text, list) {
     var line = trim(text);
+    var arrName = [];
     for (const name of list) {
         var keyWord = trim(name);
         if (line.indexOf(keyWord) >= 0) {
-            return name;
+            arrName.push(name);
         }
+    }
+
+    if (arrName.length == 1) {
+        return arrName[0];
+    } else if (arrName.length > 1) {
+        console.log(arrName);
+        return arrName.sort(function (a, b) {
+            return b.length - a.length;
+        })[0];
     }
 
     return null;
